20025382b0e1385c66906705860374fe4227e12b
Janessa Groves <jgroves1@asu.edu>
2023-10-04 16:19:32 -0500

    images // dark mode
    this will hopefully fix images // I started on dark mode styles !

---
+++ script.js
@@ -0,0 +1,11 @@
+// dark mode toggle
+const toggle = document.querySelector("#toggle");
+const body = document.body;
+
+toggle.addEventListener("click", () => {
+    body.classList.toggle("darkMode");
+});
+
+// add to cart
+
+// form validation
