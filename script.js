f09c6ee00615f0e2a02c7bb4d5960b72e9e7aea9
Janessa Groves <jgroves1@asu.edu>
2023-10-06 10:36:38 -0500

    cart // checkout
    Lots of JS to get the cart/checkout functioning, and some css and html as needed for consistency.

--- script.js
+++ script.js
@@ -19,28 +19,42 @@
         btns.forEach(button => {
 
             //event listener
-            button.addEventListener("click", () => {
+                button.addEventListener("click", () => {
             
             // more aquiring
-            const item = button.closest(".balls, .meds, .evo");
-            const productName = item.querySelector(".productName").textContent;   
-            const price = item.querySelector(".itemPrice").textContent;
-            const img = item.querySelector(".productImg").getAttribute("src");
+                const item = button.closest(".balls, .meds, .evo");
+                const productName = item.querySelector(".productName").textContent;   
+                const price = item.querySelector(".itemPrice").textContent;
+                const img = item.querySelector(".productImg").getAttribute("src");
 
              // create the objects/details
-            const itemDetails = {
-            name: productName,
-            price: price, 
-            img: img
-            };
+                const itemDetails = {
+                name: productName,
+                price: price, 
+                img: img
+                };
 
             // adding stuff to array
-            bag.push(itemDetails);
+                bag.push(itemDetails);
 
+            // add to subtotal
+                subtotal += parseInt(itemDetails.price);
+
+            // temp style the add to bag btn
+                button.classList.add("addedToBag");
+                button.innerHTML = "Added!";
+
+                // the temp timer
+                setTimeout(() => {
+                    button.classList.remove("addedToBag");
+                    button.innerHTML = "Add to Bag";
+                    }, 1500);
+                
             // show in the shopping list
                 displayShoppingList();
                 console.log(bag);
-             });
+            });
+            
         });
 
     // well first we have to clear the list
@@ -49,29 +63,92 @@
          
          bagList.innerHTML = "";
         
-    // now we make a loop
-        bag.forEach(item => {
-            const div = document.createElement("div");
-            div.classList.add("itemCard");
-            div.innerHTML = `
-            <div class="cartProductName">${item.name}</div>
-            <div class="cartProductImg"><img src="${item.img}"</div>
-            <div class="cartItemPrice">${item.price}</div>
-            `;
+        // now we make a loop
+            bag.forEach(item => {
+                const div = document.createElement("div");
+                div.classList.add("itemCard");
+                div.innerHTML = `
+                    <div class="cartProductName">${item.name}</div>
+                    <div class="cartProductImg"><img src="${item.img}"></div>
+                    <div class="cartItemPrice">${item.price}</div>
+                `;
 
-            bagList.appendChild(div);
-        })
-    };
+                bagList.appendChild(div);
+            })
+
+        // update subtotal
+            subtotalP.innerHTML = `
+            <img src="images/PokeCoin.png" class="checkoutCoin"> ${subtotal}
+            `;
+        
+        // update total
+            updateTotal();
+        };
 
     // remove items from cart ??
 
-    // bag math functions
-        // calc subtotal
-        // add tax
-        // add flat rate shipping
-        // calc total
+    // bag math
+        // declare the rates
+            const taxRate = 0.065;
+            const shipping = 50;
+            let tax = 0; 
+            let subtotal = 0;
+            let total = 0;
+
+        // outputPs (even though they are spans)
+            const subtotalP = document.querySelector(".subtotal");
+            const taxP = document.querySelector(".tax");
+            const shippingP = document.querySelector(".shipping");
+            const totalP = document.querySelector(".total");
+               
+        function updateTotal() {
+            // first calc tax since subtotal is already taken care of
+            let tax = subtotal * taxRate;
+
+            // outputP
+            taxP.innerHTML = `
+                <img src="images/PokeCoin.png" class="checkoutCoin"> ${tax.toFixed(0)}
+            `;
+            // please note that I rounded because there aren't partial cents in Poke coins
+
+            // next output the shipping rate (currently flat rate 50 pokecoins)
+            shippingP.innerHTML = `
+                <img src="images/PokeCoin.png" class="checkoutCoin"> ${shipping.toFixed(0)}
+            `;
+
+            // calculate the total
+            let total = subtotal + tax + shipping;
+
+            // output P
+            totalP.innerHTML = `
+                <img src="images/PokeCoin.png" class="checkoutCoin"> ${total.toFixed(0)}
+            `;
+        }
+        // I may have over commented on this function lol
 
     // checkout (clears bag)
+        const checkoutBtn = document.querySelector("#checkoutBtn");
+        checkoutBtn.addEventListener("click", checkout);
+
+        function checkout() {
+            // clear cart
+            bag = [];
+
+            // clear DOM
+            displayShoppingList();
+
+            // reset the math fields
+            subtotal = 0;
+
+            // reset the outputPs
+            subtotalP.textContent = subtotal;
+            taxP.textContent = tax;
+            shippingP.innerHTML = "0";
+            totalP.textContent = total;
+
+            // alert/kthnxbai
+            alert("Thank you for your order, trainer!");    
+        }
 
 // form validation
     // acquire the elements needed
